2e25f71b2d5930f37cd23cd38e1913d2b4a65c31
shrikant-sudo <55043648+shrikant-sudo@users.noreply.github.com>
2024-06-23 01:46:00 +0530

    new project added

---
+++ planner-budget/src/main/java/com/bolster/planner/budget/PlannerBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.bolster.planner.budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerBudgetApplication.class, args);
+	}
+
+}
---
+++ planner-budget/src/test/java/com/bolster/planner/budget/PlannerBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolster.planner.budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
